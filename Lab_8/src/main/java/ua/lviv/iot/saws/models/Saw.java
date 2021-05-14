d1ea74954bbd2f6fd58e8646aa9278f8b13f6a8a
max-dmytryshyn <maksumus22@gmail.com>
2021-05-14 14:26:54 +0300

    Add fields to Saw class

--- Lab_8/src/main/java/ua/lviv/iot/saws/models/Saw.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/models/Saw.java
@@ -1,5 +1,9 @@
 package ua.lviv.iot.saws.models;
 
 public class Saw {
-
+    protected MaterialToSaw materialToSaw;
+    protected DriveType driveType;
+    protected SawMaterial sawMaterial;
+    protected Person user;
+    protected Double lengthInCm;
 }
