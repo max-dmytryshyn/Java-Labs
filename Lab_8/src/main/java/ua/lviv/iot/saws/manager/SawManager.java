31ed55c9f79ec5b6467663bc4545ef23d3bf0feb
max-dmytryshyn <maksumus22@gmail.com>
2021-05-14 17:19:10 +0300

    Add default constructor to SawManager

--- Lab_8/src/main/java/ua/lviv/iot/saws/manager/SawManager.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/manager/SawManager.java
@@ -2,15 +2,20 @@ package ua.lviv.iot.saws.manager;
 
 import ua.lviv.iot.saws.models.Saw;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SawManager {
     private List<Saw> saws;
 
+    public  SawManager(){
+        this.saws = new ArrayList<Saw>();
+    }
+
     public SawManager(List<Saw> saws){
         this.saws = saws;
     }
-    
+
     public void addSaws(List<Saw> sawsToAdd){
         saws.addAll(sawsToAdd);
     }
