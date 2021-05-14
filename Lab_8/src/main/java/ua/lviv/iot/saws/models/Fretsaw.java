c82b9e1364334a490e9664503ad4c086b5c7987f
max-dmytryshyn <maksumus22@gmail.com>
2021-05-14 16:34:59 +0300

    Add getters, setters, toString(), equals(), hashCode() methods, no args constructor to Chainsaw, Fretsaw, Hacksaw, RippingSaw, TwoManSaw classes with the help of lombok annotations

--- Lab_8/src/main/java/ua/lviv/iot/saws/models/Fretsaw.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/models/Fretsaw.java
@@ -1,5 +1,10 @@
 package ua.lviv.iot.saws.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Fretsaw {
     private Double arcLengthInCm;
     private String bladeManufacturer;
--- Lab_8/src/main/java/ua/lviv/iot/saws/models/RippingSaw.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/models/RippingSaw.java
@@ -1,5 +1,10 @@
 package ua.lviv.iot.saws.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RippingSaw {
     private String toothShape;
 }
