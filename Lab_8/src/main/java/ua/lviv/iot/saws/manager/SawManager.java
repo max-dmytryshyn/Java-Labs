73a9a42cfe994d7649b1351d7d113337f081e6be
max-dmytryshyn <maksumus22@gmail.com>
2021-05-14 17:09:12 +0300

    Add SortOrder enum and SawManager class with field and constructor to manager package

---
+++ Lab_8/src/main/java/ua/lviv/iot/saws/manager/SawManager.java
@@ -0,0 +1,14 @@
+package ua.lviv.iot.saws.manager;
+
+import ua.lviv.iot.saws.models.Saw;
+
+import java.util.List;
+
+public class SawManager {
+    private List<Saw> saws;
+
+    public SawManager(List<Saw> saws){
+        this.saws = saws;
+    }
+
+}
---
+++ Lab_8/src/main/java/ua/lviv/iot/saws/manager/SortOrder.java
@@ -0,0 +1,6 @@
+package ua.lviv.iot.saws.manager;
+
+public enum SortOrder {
+    ASC,
+    DESC
+}
