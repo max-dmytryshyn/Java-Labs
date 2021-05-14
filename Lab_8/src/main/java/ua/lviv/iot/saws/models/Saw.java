d4b1f1a1424925fb668195121bb792643a304620
max-dmytryshyn <maksumus22@gmail.com>
2021-05-14 16:12:10 +0300

    Add getters, setters, toString(), equals(), hashCode() methods, all and no args constructors to Saw class with the help of lombok annotations

--- Lab_8/src/main/java/ua/lviv/iot/saws/models/Saw.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/models/Saw.java
@@ -1,5 +1,13 @@
 package ua.lviv.iot.saws.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Saw {
     protected MaterialToSaw materialToSaw;
     protected DriveType driveType;
