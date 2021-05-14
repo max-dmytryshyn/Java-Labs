e7ee156e1f0790379cee1d2e84a606971c07b6b8
max-dmytryshyn <maksumus22@gmail.com>
2021-05-14 16:25:11 +0300

    Add getters, setters, toString(), equals(), hashCode() methods, no args constructor to MechanicalSaw class with the help of lombok annotations

--- Lab_8/src/main/java/ua/lviv/iot/saws/models/MechanicalSaw.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/models/MechanicalSaw.java
@@ -1,5 +1,12 @@
 package ua.lviv.iot.saws.models;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class MechanicalSaw extends Saw{
     private Double toothSizeInMm;
+
 }
