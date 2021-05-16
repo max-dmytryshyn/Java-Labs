5550caff8f6e8fa0ee1f4184eb9fa17bda101df4
max-dmytryshyn <maksumus22@gmail.com>
2021-05-16 20:22:01 +0300

    Add create, update methods and id assignment logic to SawService

--- Lab_9/src/main/java/ua/lviv/iot/saws/services/SawService.java
+++ Lab_9/src/main/java/ua/lviv/iot/saws/services/SawService.java
@@ -6,10 +6,12 @@ import ua.lviv.iot.saws.models.Saw;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Service
 public class SawService {
+    private AtomicInteger id = new AtomicInteger(0);
     private Map<Integer, Saw> sawsMap = new HashMap<Integer, Saw>();
 
     public List<Saw> getSaws(){
@@ -19,4 +21,15 @@ public class SawService {
     public Saw getSawById(Integer id) {
         return sawsMap.get(id);
     }
+
+    public Saw createSaw(Saw saw){
+        saw.setId(id.incrementAndGet());
+        sawsMap.put(saw.getId(), saw);
+        return saw;
+    }
+
+    public Saw updateSawById(Integer id, Saw saw){
+        return sawsMap.put(id, saw);
+    }
+
 }
