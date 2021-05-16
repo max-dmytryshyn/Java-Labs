a05b6e326bc1f03e30e2c5522f83f00b462fb4da
max-dmytryshyn <maksumus22@gmail.com>
2021-05-16 20:37:15 +0300

    Add @NotNull annotation to necessary fields so they mustn't be null

--- Lab_9/src/main/java/ua/lviv/iot/saws/models/Saw.java
+++ Lab_9/src/main/java/ua/lviv/iot/saws/models/Saw.java
@@ -5,15 +5,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Saw {
+    @NonNull
     protected MaterialToSaw materialToSaw;
+    @NonNull
     protected DriveType driveType;
+    @NonNull
     protected SawMaterial sawMaterial;
     protected Person user;
+    @NonNull
     protected Double lengthInCm;
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     protected Integer id;
