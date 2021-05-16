598166c12beb5ba606d5c11c5446c5fdfd0ca359
max-dmytryshyn <maksumus22@gmail.com>
2021-05-16 18:34:28 +0300

    Add @ResrController anotation, site paths, get by id and get all methods to SawController

--- Lab_9/src/main/java/ua/lviv/iot/saws/controllers/SawController.java
+++ Lab_9/src/main/java/ua/lviv/iot/saws/controllers/SawController.java
@@ -1,4 +1,29 @@
 package ua.lviv.iot.saws.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.lviv.iot.saws.models.*;
+import ua.lviv.iot.saws.services.SawService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/saws")
 public class SawController {
+
+    @Autowired
+    private SawService sawService;
+
+    @GetMapping(path = "/{id}")
+    public Saw getSawById(@PathVariable Integer id){
+        return sawService.getSawById(id);
+    }
+
+    @GetMapping
+    public List<Saw> getSaws(){
+        return sawService.getSaws();
+    }
 }
