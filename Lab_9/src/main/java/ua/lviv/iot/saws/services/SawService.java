b4928359233c92ff590ba670a156e07c5c0ff212
max-dmytryshyn <maksumus22@gmail.com>
2021-05-16 18:33:50 +0300

    Add @Service annotation, get by id and get all methods to SawService

--- Lab_9/src/main/java/ua/lviv/iot/saws/services/SawService.java
+++ Lab_9/src/main/java/ua/lviv/iot/saws/services/SawService.java
@@ -1,4 +1,22 @@
 package ua.lviv.iot.saws.services;
 
+import org.springframework.stereotype.Service;
+import ua.lviv.iot.saws.models.Saw;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class SawService {
+    private Map<Integer, Saw> sawsMap = new HashMap<Integer, Saw>();
+
+    public List<Saw> getSaws(){
+        return sawsMap.values().stream().collect(Collectors.toList());
+    }
+
+    public Saw getSawById(Integer id) {
+        return sawsMap.get(id);
+    }
 }
