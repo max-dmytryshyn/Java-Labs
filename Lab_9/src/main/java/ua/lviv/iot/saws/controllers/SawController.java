894138cc04cab070edd806c5fc90716591a304bf
max-dmytryshyn <maksumus22@gmail.com>
2021-05-16 21:44:32 +0300

    Add method to process DELETE reqiest

--- Lab_9/src/main/java/ua/lviv/iot/saws/controllers/SawController.java
+++ Lab_9/src/main/java/ua/lviv/iot/saws/controllers/SawController.java
@@ -2,16 +2,11 @@ package ua.lviv.iot.saws.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.lviv.iot.saws.models.Saw;
 import ua.lviv.iot.saws.services.SawService;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 @RestController
@@ -46,8 +41,20 @@ public class SawController {
     public ResponseEntity<Saw> updateSawById(@PathVariable Integer id, @RequestBody Saw saw){
         Saw existing_saw = sawService.getSawById(id);
         if(existing_saw != null){
-            sawService.updateSawById(id, saw);
+            ResponseEntity.ok(sawService.updateSawById(id, saw));
             return ResponseEntity.ok(saw);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @CrossOrigin
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Saw> deleteSawById(@PathVariable Integer id){
+         Saw existing_saw = sawService.getSawById(id);
+        if(existing_saw != null){
+            return ResponseEntity.ok(sawService.deleteSawById(id));
         }
         else {
             return ResponseEntity.notFound().build();
