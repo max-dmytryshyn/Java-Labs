cd7b6faaaae3b26b424a503e82c2b6cbdb2bc07f
max-dmytryshyn <maksumus22@gmail.com>
2021-05-17 19:15:16 +0300

    Make Saw constructor public and code more readeable

--- Lab_9/src/main/java/ua/lviv/iot/saws/models/Saw.java
+++ Lab_9/src/main/java/ua/lviv/iot/saws/models/Saw.java
@@ -11,19 +11,25 @@ import lombok.NonNull;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Saw {
+
     @NonNull
     protected MaterialToSaw materialToSaw;
+
     @NonNull
     protected DriveType driveType;
+
     @NonNull
     protected SawMaterial sawMaterial;
+
     protected Person user;
+
     @NonNull
     protected Double lengthInCm;
+
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     protected Integer id;
 
-    Saw(final MaterialToSaw materialToSaw, final DriveType driveType, final SawMaterial sawMaterial,
+    public Saw(final MaterialToSaw materialToSaw, final DriveType driveType, final SawMaterial sawMaterial,
         final Person person, final Double lengthInCm) {
         this(materialToSaw, driveType, sawMaterial, person, lengthInCm, null);
     }
