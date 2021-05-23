c9a32b170b5bc50e327d7779de717802e3a8124c
max-dmytryshyn <maksumus22@gmail.com>
2021-05-23 14:42:24 +0300

    Create exceptions package with IdProvidedWhileCreationExeption exeption

---
+++ Lab_10/src/main/java/ua/lviv/iot/exeptions/IdProvidedWhileCreationException.java
@@ -0,0 +1,9 @@
+package ua.lviv.iot.exeptions;
+
+public class IdProvidedWhileCreationException extends RuntimeException {
+
+    @Override
+    public String getMessage(){
+        return "Failed to create a saw with passed id. Creation method should not use external ids";
+    }
+}
