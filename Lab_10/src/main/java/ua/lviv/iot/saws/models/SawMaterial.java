46d1573bfe85dc286b67bf04607a534fd388f61f
max-dmytryshyn <maksumus22@gmail.com>
2021-05-23 13:54:33 +0300

    Make SawMaterial and Person embeddable(allow create colums from their fields in data base without creation of tables)

--- Lab_10/src/main/java/ua/lviv/iot/saws/models/SawMaterial.java
+++ Lab_10/src/main/java/ua/lviv/iot/saws/models/SawMaterial.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Embeddable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class SawMaterial {
+
     private String handleMaterial;
+
     private String bladeMaterial;
+
 }
