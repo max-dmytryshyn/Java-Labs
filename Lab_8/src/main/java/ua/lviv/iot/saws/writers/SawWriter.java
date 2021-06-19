9ea16ef550921ad51232f8b02f380031823baba1
max-dmytryshyn <maksumus22@gmail.com>
2021-06-19 22:42:56 +0300

    Create SawWriter class which writes objects of Saw class to CSV file

---
+++ Lab_8/src/main/java/ua/lviv/iot/saws/writers/SawWriter.java
@@ -0,0 +1,56 @@
+package ua.lviv.iot.saws.writers;
+
+import ua.lviv.iot.saws.models.Saw;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+
+public class SawWriter {
+    public static void writeToFile(final String filePath, final List<Saw> saws) {
+            try {
+                File csvOutputFile = new File(filePath);
+                System.out.println("Creating a " + filePath);
+                if (csvOutputFile.createNewFile()) {
+                    System.out.println("File " + filePath + " created");
+                } else {
+                    System.out.println("File " + filePath + " already exists");
+                }
+            } catch (IOException e) {
+                System.out.println("Error occurred. Cannot create a file in " + filePath);
+                e.printStackTrace();
+            }
+        if (!saws.isEmpty()) {
+            try {
+                FileWriter csvOutputFileWriter = new FileWriter(filePath);
+                System.out.println("Writing to " + filePath);
+                saws.sort(Comparator.comparing(saw -> saw.getClass().getSimpleName()));
+                String currentHeader = saws.get(0).getHeaders();
+                csvOutputFileWriter.write(currentHeader + "\n");
+                for (Saw saw: saws){
+                    try {
+                        if (!saw.getHeaders().equals(currentHeader)){
+                            csvOutputFileWriter.write(currentHeader + "\n");
+                            currentHeader = saw.getHeaders();
+                        }
+                        csvOutputFileWriter.write(saw.toCSV() + '\n');
+                    } catch (IOException e) {
+                        System.out.println("Error occurred. Cannot write to " + filePath);
+                        e.printStackTrace();
+                    }
+                }
+                csvOutputFileWriter.close();
+                System.out.println("Successfully wrote to " + filePath);
+            } catch (IOException e) {
+                System.out.println("Error occurred. Cannot create file writer for " + filePath);
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println("No saws to write");
+        }
+    }
+}
+
