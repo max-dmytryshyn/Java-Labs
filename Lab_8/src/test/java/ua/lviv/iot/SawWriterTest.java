dde23a9734560cf9c91db7cd8def034bf2f0dd59
max-dmytryshyn <maksumus22@gmail.com>
2021-06-19 22:51:46 +0300

    Write test for SawWriter

---
+++ Lab_8/src/test/java/ua/lviv/iot/SawWriterTest.java
@@ -0,0 +1,74 @@
+package ua.lviv.iot;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import ua.lviv.iot.saws.models.*;
+import ua.lviv.iot.saws.writers.SawWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SawWriterTest {
+
+    @Test
+    void test_convert_to_csv_all_types_of_saw(){
+        AtomicInteger sawId = new AtomicInteger(1);
+        ArrayList<Saw> saws = new ArrayList<>();
+        saws.add(new Jigsaw(sawId.getAndIncrement(), new SawMaterial("plastic", "copper"), 0, 14.5, 240));
+        saws.add(new Chainsaw(sawId.getAndIncrement(), new SawMaterial("plastic", "steel"), 1, 30.0, 5.5));
+        saws.add(new RippingSaw(sawId.getAndIncrement(), new SawMaterial("wood", "lead"), 2, 16.0, 5.0, "round"));
+        saws.add(new Hacksaw(sawId.getAndIncrement(), new SawMaterial("plastic", "steel"), 3, 12.0, 10.0, "Blades CO"));
+        saws.add(new TwoManSaw(sawId.getAndIncrement(), new SawMaterial("wood", "steel"), 0, 40.5, 6.0, 5));
+        saws.add(new Hacksaw(sawId.getAndIncrement(), new SawMaterial("wood", "steel"), 5, 13.5, 8.25, "Blades CO"));
+        saws.add(new RippingSaw(sawId.getAndIncrement(), new SawMaterial("plastic", "lead"), 2, 9.0, 5.0, "rectangle"));
+        saws.add(new Jigsaw(sawId.getAndIncrement(), new SawMaterial("plastic", "steel"), 1, 12.5, 240));
+        saws.add(new Chainsaw(sawId.getAndIncrement(), new SawMaterial("plastic", "steel"), 2, 35.5, 3.5));
+        saws.add(new Fretsaw(
+                sawId.getAndIncrement(), new SawMaterial("wood", "aluminium"), 4, 13.2, 7.0, 20.5, "Blades CO"
+        ));
+        SawWriter.writeToFile("src/test/resources/SAWS.csv", saws);
+        File result = new File("src/test/resources/SAWS.csv");
+        File expected = new File("src/test/resources/ALL-SAWS-SAMPLE.csv");
+        try {
+            assertEquals(true, FileUtils.contentEquals(result, expected));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void test_convert_to_csv_one_type_of_saw(){
+        AtomicInteger sawId = new AtomicInteger(1);
+        ArrayList<Saw> saws = new ArrayList<>();
+        saws.add(new Jigsaw(sawId.getAndIncrement(), new SawMaterial("plastic", "copper"), 0, 14.5, 240));
+        saws.add(new Jigsaw(sawId.getAndIncrement(), new SawMaterial("plastic", "steel"), 1, 12.5, 240));
+        saws.add(new Jigsaw(sawId.getAndIncrement(), new SawMaterial("plastic", "aluminium"), 2, 10.5, 240));
+        SawWriter.writeToFile("src/test/resources/SAWS.csv", saws);
+        File result = new File("src/test/resources/SAWS.csv");
+        File expected = new File("src/test/resources/ONE-TYPE-SAWS-SAMPLE.csv");
+        try {
+            assertEquals(true, FileUtils.contentEquals(result, expected));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void test_convert_to_csv_no_saws(){
+        File expected = new File("src/test/resources/SAWS.csv");
+        ArrayList<Saw> saws = new ArrayList<>();
+        SawWriter.writeToFile("src/test/resources/SAWS.csv", saws);
+        File result = new File("src/test/resources/SAWS.csv");
+        try {
+            assertEquals(true, FileUtils.contentEquals(result, expected));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
