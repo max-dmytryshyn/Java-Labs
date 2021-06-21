12a4a5ab8f60e3d4c29bc480e84bade3d3480bbb
max-dmytryshyn <maksumus22@gmail.com>
2021-06-21 09:34:08 +0300

    Rewrite if statement which checks if provided list is empty to make it more readable

--- Lab_8/src/main/java/ua/lviv/iot/saws/writers/PersonWriter.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/writers/PersonWriter.java
@@ -9,26 +9,26 @@ import java.util.List;
 
 public class PersonWriter {
     public static void writeToFile(String filePath, List<Person> people) {
-        if (!people.isEmpty()) {
-            try (FileWriter csvOutputFileWriter = new FileWriter(filePath)) {
-                System.out.println("Writing to " + filePath);
-                csvOutputFileWriter.write(people.get(0).getHeaders() + "\n");
-                people.forEach(person -> {
-                    try {
-                        csvOutputFileWriter.write(person.toCSV() + '\n');
-                    } catch (IOException e) {
-                        System.out.println("Error occurred. Cannot write to " + filePath);
-                        e.printStackTrace();
-                    }
-                });
-                csvOutputFileWriter.close();
-                System.out.println("Successfully wrote to " + filePath);
-            } catch (IOException e) {
-                System.out.println("Error occurred. Cannot create file writer for " + filePath);
-                e.printStackTrace();
-            }
-        } else {
+        if (people.isEmpty()) {
             System.out.println("No people to write");
+            return;
+        }
+        try (FileWriter csvOutputFileWriter = new FileWriter(filePath)) {
+            System.out.println("Writing to " + filePath);
+            csvOutputFileWriter.write(people.get(0).getHeaders() + "\n");
+            people.forEach(person -> {
+                try {
+                    csvOutputFileWriter.write(person.toCSV() + '\n');
+                } catch (IOException e) {
+                    System.out.println("Error occurred. Cannot write to " + filePath);
+                    e.printStackTrace();
+                }
+            });
+            csvOutputFileWriter.close();
+            System.out.println("Successfully wrote to " + filePath);
+        } catch (IOException e) {
+            System.out.println("Error occurred. Cannot create file writer for " + filePath);
+            e.printStackTrace();
         }
     }
 }
--- Lab_8/src/main/java/ua/lviv/iot/saws/writers/SawWriter.java
+++ Lab_8/src/main/java/ua/lviv/iot/saws/writers/SawWriter.java
@@ -10,32 +10,32 @@ import java.util.List;
 
 public class SawWriter {
     public static void writeToFile(final String filePath, final List<Saw> saws) {
-        if (!saws.isEmpty()) {
-            try (FileWriter csvOutputFileWriter = new FileWriter(filePath)) {
-                System.out.println("Writing to " + filePath);
-                saws.sort(Comparator.comparing(saw -> saw.getClass().getSimpleName()));
-                String currentHeader = saws.get(0).getHeaders();
-                csvOutputFileWriter.write(currentHeader + "\n");
-                for (Saw saw: saws) {
-                    try {
-                        if (!saw.getHeaders().equals(currentHeader)) {
-                            csvOutputFileWriter.write(currentHeader + "\n");
-                            currentHeader = saw.getHeaders();
-                        }
-                        csvOutputFileWriter.write(saw.toCSV() + '\n');
-                    } catch (IOException e) {
-                        System.out.println("Error occurred. Cannot write to " + filePath);
-                        e.printStackTrace();
-                    }
-                }
-                csvOutputFileWriter.close();
-                System.out.println("Successfully wrote to " + filePath);
-            } catch (IOException e) {
-                System.out.println("Error occurred. Cannot create file writer for " + filePath);
-                e.printStackTrace();
-            }
-        } else {
+        if (saws.isEmpty()) {
             System.out.println("No saws to write");
+            return;
+        }
+        try (FileWriter csvOutputFileWriter = new FileWriter(filePath)) {
+            System.out.println("Writing to " + filePath);
+            saws.sort(Comparator.comparing(saw -> saw.getClass().getSimpleName()));
+            String currentHeader = saws.get(0).getHeaders();
+            csvOutputFileWriter.write(currentHeader + "\n");
+            for (Saw saw : saws) {
+                try {
+                    if (!saw.getHeaders().equals(currentHeader)) {
+                        csvOutputFileWriter.write(currentHeader + "\n");
+                        currentHeader = saw.getHeaders();
+                    }
+                    csvOutputFileWriter.write(saw.toCSV() + '\n');
+                } catch (IOException e) {
+                    System.out.println("Error occurred. Cannot write to " + filePath);
+                    e.printStackTrace();
+                }
+            }
+            csvOutputFileWriter.close();
+            System.out.println("Successfully wrote to " + filePath);
+        } catch (IOException e) {
+            System.out.println("Error occurred. Cannot create file writer for " + filePath);
+            e.printStackTrace();
         }
     }
 }
